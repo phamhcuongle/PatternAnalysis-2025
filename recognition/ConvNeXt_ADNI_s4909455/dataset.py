af22de8c4ba810cfee5bdb9b54c28d4c0793fe3c
phamhcuongle <phamhungcuongle@gmail.com>
2025-10-31 02:27:36 +1000

    Refactor dataset loading

--- recognition/ConvNeXt_ADNI_s4909455/dataset.py
+++ recognition/ConvNeXt_ADNI_s4909455/dataset.py
@@ -1,114 +1,141 @@
+import os
 from pathlib import Path
-import random
-from typing import List, Tuple
-
+from collections import defaultdict
 import torch
 from torch.utils.data import Dataset, DataLoader
-from PIL import Image
 from torchvision import transforms
-
-
-def _get_subject_id(fname: str) -> str:
-    return Path(fname).stem.split('_')[0]
+from torchvision.transforms import v2
+from PIL import Image
+import numpy as np
 
 
 class ADNIDataset(Dataset):
-    def __init__(self, items: List[Tuple[Path, int]], transform=None):
-        self.items = items
+    def __init__(self, root_dir, split='train', transform=None, subject_ids=None):
+        self.root_dir = Path(root_dir)
+        self.split = split
         self.transform = transform
-
-    def __len__(self):
-        return len(self.items)
-
-    def __getitem__(self, idx):
-        path, label = self.items[idx]
-        img = Image.open(path).convert('RGB')
-        if self.transform:
-            img = self.transform(img)
-        return img, label
-
-
-def build_subject_splits(root: str, train_dir_name='train', val_ratio=0.15, seed=42):
-    root = Path(root)
-    train_root = root / 'AD_NC' / train_dir_name
-    classes = ['AD', 'NC']
-
-    subjects_per_class = {}
-    files_per_subject = {}
-
-    for cls in classes:
-        cls_dir = train_root / cls
-        if not cls_dir.exists():
-            continue
-        files = sorted(p for p in cls_dir.iterdir() if p.suffix.lower() in ('.jpg', '.jpeg', '.png'))
-        subj_map = {}
-        for f in files:
-            sid = _get_subject_id(f.name)
-            subj_map.setdefault(sid, []).append(f)
-        subjects_per_class[cls] = list(subj_map.keys())
-        files_per_subject.update({(cls, sid): subj_map[sid] for sid in subj_map})
-
-    random.seed(seed)
-    train_items = []
-    val_items = []
-    for cls in classes:
-        sids = subjects_per_class.get(cls, [])
-        random.shuffle(sids)
-        n_val = max(1, int(len(sids) * val_ratio))
-        val_sids = set(sids[:n_val])
-        label = 0 if cls == 'AD' else 1
-        for sid in sids:
-            for p in files_per_subject[(cls, sid)]:
-                if sid in val_sids:
-                    val_items.append((p, label))
-                else:
-                    train_items.append((p, label))
-
-    return train_items, val_items
-
-
-def build_dataloaders(root: str, batch_size: int = 64, image_size: int = 224, val_ratio: float = 0.15,
-                      num_workers: int = 4, seed: int = 42):
-    train_items, val_items = build_subject_splits(root, val_ratio=val_ratio, seed=seed)
-
-    imagenet_mean = [0.485, 0.456, 0.406]
-    imagenet_std = [0.229, 0.224, 0.225]
-
-    train_transform = transforms.Compose([
-        transforms.RandomResizedCrop(image_size, scale=(0.8, 1.0)),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(degrees=15),
-        transforms.RandomApply([transforms.RandAugment(num_ops=2, magnitude=9)], p=0.5),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=imagenet_mean, std=imagenet_std)
-    ])
-
-    val_transform = transforms.Compose([
-        transforms.Resize(int(image_size * 256 / 224)),
-        transforms.CenterCrop(image_size),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=imagenet_mean, std=imagenet_std)
-    ])
-
-    train_ds = ADNIDataset(train_items, transform=train_transform)
-    val_ds = ADNIDataset(val_items, transform=val_transform)
-
-    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
-    val_loader = DataLoader(val_ds, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
-
-    return train_loader, val_loader
-
-
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--root', type=str, default='./ADNI')
-    parser.add_argument('--batch-size', type=int, default=8)
-    parser.add_argument('--num-workers', type=int, default=2)
-    args = parser.parse_args()
+        self.samples = []
+        self.class_to_idx = {'NC': 0, 'AD': 1}
+        
+        if split == 'test':
+            self._load_test_data()
+        else:
+            self._load_train_val_data(subject_ids)
     
-    if not Path(args.root).exists():
-        print(f"Error: Data directory not found at {args.root}")
+    def _load_test_data(self):
+        test_dir = self.root_dir / 'test'
+        for class_name in ['AD', 'NC']:
+            class_dir = test_dir / class_name
+            if class_dir.exists():
+                for img_path in sorted(class_dir.glob('*.jpeg')):
+                    self.samples.append((str(img_path), self.class_to_idx[class_name]))
+    
+    def _load_train_val_data(self, subject_ids):
+        train_dir = self.root_dir / 'train'
+        for class_name in ['AD', 'NC']:
+            class_dir = train_dir / class_name
+            if class_dir.exists():
+                for img_path in sorted(class_dir.glob('*.jpeg')):
+                    subject_id = self._extract_subject_id(img_path.name)
+                    if subject_ids is None or subject_id in subject_ids:
+                        self.samples.append((str(img_path), self.class_to_idx[class_name]))
+    
+    @staticmethod
+    def _extract_subject_id(filename):
+        return filename.split('_')[0]
+    
+    def __len__(self):
+        return len(self.samples)
+    
+    def __getitem__(self, idx):
+        img_path, label = self.samples[idx]
+        image = Image.open(img_path).convert('RGB')
+        
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, label
+
+
+def create_subject_split(root_dir, train_ratio=0.9, seed=42):
+    train_dir = Path(root_dir) / 'train'
+    subject_ids_per_class = defaultdict(set)
+    
+    for class_name in ['AD', 'NC']:
+        class_dir = train_dir / class_name
+        if class_dir.exists():
+            for img_path in class_dir.glob('*.jpeg'):
+                subject_id = ADNIDataset._extract_subject_id(img_path.name)
+                subject_ids_per_class[class_name].add(subject_id)
+    
+    train_subjects = []
+    val_subjects = []
+    
+    np.random.seed(seed)
+    for class_name, subject_ids in subject_ids_per_class.items():
+        subject_list = sorted(list(subject_ids))
+        np.random.shuffle(subject_list)
+        
+        n_train = int(len(subject_list) * train_ratio)
+        train_subjects.extend(subject_list[:n_train])
+        val_subjects.extend(subject_list[n_train:])
+    
+    return set(train_subjects), set(val_subjects)
+
+
+def build_transform(is_train, input_size=224):
+    if is_train:
+        transform = transforms.Compose([
+            transforms.Resize((input_size, input_size)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandAugment(num_ops=9, magnitude=9),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            transforms.RandomErasing(p=0.25)
+        ])
     else:
-        tr, va = build_dataloaders(args.root, batch_size=args.batch_size, image_size=224, num_workers=args.num_workers)
-        print(f'train batches: {len(tr)}, val batches: {len(va)}')
+        transform = transforms.Compose([
+            transforms.Resize((input_size, input_size)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+    
+    return transform
+
+
+def build_loader(root_dir, batch_size=32, num_workers=4, input_size=224, seed=42):
+    train_subjects, val_subjects = create_subject_split(root_dir, train_ratio=0.9, seed=seed)
+    
+    train_transform = build_transform(is_train=True, input_size=input_size)
+    val_transform = build_transform(is_train=False, input_size=input_size)
+    
+    train_dataset = ADNIDataset(root_dir, split='train', transform=train_transform, subject_ids=train_subjects)
+    val_dataset = ADNIDataset(root_dir, split='train', transform=val_transform, subject_ids=val_subjects)
+    test_dataset = ADNIDataset(root_dir, split='test', transform=val_transform)
+    
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True,
+        drop_last=True
+    )
+    
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=True
+    )
+    
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=True
+    )
+    
+    return train_loader, val_loader, test_loader
